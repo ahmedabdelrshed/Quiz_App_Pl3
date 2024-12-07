c05f2d687c37408b38d6044403c4c8c239f7946b
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2024-12-07 19:15:41 +0200

    create form ui quiz app

--- Program.fs
+++ Program.fs
@@ -1,16 +1,24 @@
 ﻿open System
-open System.Windows.Forms
 open System.Drawing
+open System.Windows.Forms
+// Form UI for Quiz App
+let form = new Form(Text = "Quiz Application", Size = Size(800, 600))
+form.BackColor <- Color.DeepSkyBlue
+form.StartPosition <- FormStartPosition.CenterScreen
+let questionLabel = new Label(Text = "", Location = Point(10, 10), Size = Size(450, 50), ForeColor = Color.Brown, TextAlign = ContentAlignment.MiddleCenter)
+questionLabel.BackColor <- Color.Beige
+questionLabel.Location <- Point((form.ClientSize.Width - questionLabel.Width) / 2, (10))
+let answerBox = new TextBox(Location = Point(160, 70), Size = Size(450, 50))
+let optionsPanel = new Panel(Location = Point(160, 70), Size = Size(450, 120))
+let timerLabel = new Label(Text = "Time: 10", Location = Point(160, 200), Size = Size(100, 30), ForeColor = Color.Red)
+let submitButton = new Button(Text = "Submit", Location = Point(160, 230), Size = Size(150, 30))
+submitButton.BackColor <- Color.SeaGreen
+submitButton.ForeColor <- Color.WhiteSmoke
+let resultLabel = new Label(Text = "", Location = Point(10, 270), Size = Size(450, 50),TextAlign = ContentAlignment.MiddleCenter)
+resultLabel.Location <- Point((form.ClientSize.Width - resultLabel.Width) / 2, 
+                              (form.ClientSize.Height - resultLabel.Height) / 2)
+resultLabel.BackColor <- Color.Honeydew
+resultLabel.Visible <- false
+form.Controls.AddRange([| questionLabel; answerBox; optionsPanel; timerLabel; submitButton; resultLabel |])
 
-[<EntryPoint>]
-let main _ =
-    // Create a new form
-    let form = new Form(Text = "Hello World Form", Width = 300, Height = 200)
-
-    // Create a label and add it to the form
-    let label = new Label(Text = "Hello, World! From Quiz App", Dock = DockStyle.Fill, TextAlign = ContentAlignment.MiddleCenter)
-    form.Controls.Add(label)
-
-    // Run the application
-    Application.Run(form)
-    0
+Application.Run(form)
